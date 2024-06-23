57bae0887798b806c98b29df8d394e176c3f3ac9
MCYP_MacStudio <shhcyp@qq.com>
2024-06-23 11:05:12 +0800

    Initial commit

---
+++ src/main/java/cn/wowtw_backend/WowtwBackendApplication.java
@@ -0,0 +1,13 @@
+package cn.wowtw_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WowtwBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WowtwBackendApplication.class, args);
+    }
+
+}
