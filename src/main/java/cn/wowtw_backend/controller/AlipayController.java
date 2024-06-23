b5c1b0395e7029b878180de3a418a5a0fadfcfff
MCYP_MacStudio <shhcyp@qq.com>
2024-06-23 23:43:07 +0800

    粗略完成生成预创订单二维码

--- src/main/java/cn/wowtw_backend/controller/AlipayController.java
+++ src/main/java/cn/wowtw_backend/controller/AlipayController.java
@@ -1,6 +1,7 @@
 package cn.wowtw_backend.controller;
 
 import cn.wowtw_backend.model.Order;
+import cn.wowtw_backend.utils.QRCodeUtil;
 import cn.wowtw_backend.utils.Result;
 import com.alipay.api.AlipayApiException;
 import com.alipay.api.AlipayClient;
@@ -9,24 +10,32 @@ import com.alipay.api.DefaultAlipayClient;
 import com.alipay.api.domain.AlipayTradePrecreateModel;
 import com.alipay.api.request.AlipayTradePrecreateRequest;
 import com.alipay.api.response.AlipayTradePrecreateResponse;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
+import com.google.zxing.WriterException;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.*;
+
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
 
 @Slf4j
 @RestController
 @RequestMapping("/api/alipay")
 public class AlipayController {
-    String privateKey = "MIIEvQIBADANBgkqhkiG9w0BAQEFAASCBKcwggSjAgEAAoIBAQCQ7Y50eMjkEarV9sByn/skkUy02/eewNJcLLM3ezf9ZhXPu4jWdDy5amsEyxAgdjgrZVX+/h0+nniXNCjWOFPwjj9JXpQHqpgw/9TFi6k9ocucikxviWt8gryqF2uzat8//+Yq0Ne4tZpiTi1TXQ9lNCMEePVP/LB9D/9Trpsu6J6ZPbZZpyV0Pp8G0izCXeJo3q2AG0ZuG/0EuOxX5ty1GSY5T+3+QOfvsBKKeBXjGFRzY4CBjlDTs3v4SvU8bwKc8+nVEHeL9mndXWyEF5Y5kZX09nI3B3kOkoaZcoWZfCDB80HDn/oLsMT/xKL/KYEAJscufCjhfJ2MCYWDl4PdAgMBAAECggEAS7YerpAO+r6UDbUbcoZLEB5sASvM332CJ68ebQSNCm8TQ/OHl+kfdAm5IMbohNzdmSYWV+Du7DCjdPJFxyukBk55l122XY7sdOShhhXg8xacJQpGxhn3MhOmdgFIPXG+cVPBLWru10rf/S00FpHOIiOri4GZPhEXLi52R4Q3A7iu0v9vprQMoNwwha2VwV4XC7854Vxoi4nm+HaHUyzso2N29NToD1RaolKypGbJpRP68fndK6a/UMIhDeVuuotUvoOKdGCT47shwSFNZvR33OgZOZeHEP0Lp3waOKLnHhF+4B8HVBOhXhE/kTdp4ZhaUHgmtDzs5byKBaHoZetBbQKBgQDMGE1JAFzhtKzAMttJExAIlLFvuBlYfZi44LXIyALkDwLVXNmmc3k3SuZIiJzJc3C5VhB2DfCGW8RgpPbcRp01tIgL6+m3uSh/tCvP1XR4nLZaNT38HdIKRRMhv14znc6Jk5bfGdBKqG03pOyRrRg28ZwpimspVZLXwbU9e4U+BwKBgQC1ySkOJ4QZWGRcW5RIeqC0IxM3NjpmDhrCfyYzlo7yyq4lq43ZQ+g9KrYlgZDHkNG9gqpf3kulR7eU75uTYtaGHx2YRJNx1ynYJ1Te92g51EdBv32YXO/rR6+rZu2cthy9q2lsJMxNU0mr29W76EysstQ6eWGeC5HXa6b8yfH1+wKBgF+xZUs9KE+LbxQOxFJW5cWgMjG3l+qtuNW2RoWFghV8FFAF4ObybXtHWwHXS+wIfGnpuH4huwzEKidGMZjGYx+zLbapPkQuRJbb207vPlTu5XEWVaHIOZsnF/lSyNBaxWQsFykz6T2YCt10Lpd/GUNptVak1+nhfaA76saRC8AxAoGBAJEJ48DQVHxoYwKxarj9ZjTze0Rk9ukwPbeK/S7R5FHg1ltl6jI8FRKgfl2kZ6BicoXSuB7vXMmNU/L3zdglVuYB83nm1QFSW6eDkhqnqTfin8vcfdfP5viA5K9L2y9aucm29tD69yqG1lPRfVGEgZCUyfGxoOoZbw5CbSRp96x7AoGAG2FnQ0TLNTYi0ZkTexpu2QNAuOnbRjjFk4ZQP4/mlXKRGwRALwN/ccKCeuliqyZ8RL4NkaaylsLWc8MDnAAlEcRtRDiSMUuGmMct8o3hNqhy0zctBtCuZtJyodSxqSyMwU3lSmdCcIFUmZG/Uem5pSkRVMaDW2tVBpulRtZa6O4=";
-    String alipayPublicKey = "MIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEA2d7gL+fJBE8iuqBnLrVMilIPj5YiZ8S+oh3nTLG3VQqyGmJwzFlVS/4Pal8fC0wziYn7la8YOOdXFIHg3F4GjXJ8sVZ24J1ubYcgjTipRnJmy/h3idZjNy7tkp3x6AqbZbYrtztIv1RKgucPS1s/XtVG22pYjun7e+vGaQDnO9pP3OCDTl1G/oDAoQlVlhcDNU1jbxZREy3uTD/RRl4OC5uElg97qQd6pTbEYHHt7OyGHIDa1Wr1VaHz24EyOg3NlmmzCRqdwTZeZI2NVopfkvNtU8GYyR2qYdEMAQevU1GMDw6pi4g68ug8BCqxxPU1yem3IISP8d+qUODirC5ykwIDAQAB";
+    String privateKey = "MIIEvQIBADANBgkqhkiG9w0BAQEFAASCBKcwggSjAgEAAoIBAQCDi65XrrlOXUYgpJRZaHN5x0fiPCFnLmxxVqyOtzW3g4+wuvmPqKNZx9LzwdZIs3XV79YCa8LCD0oeShYnVQyUtS/j2ECNNhcXhZMNQ9h15kr5ZVQq+nLP2/1ou/BsAZZd/817OGy0VCtQuK32O5HvAAEqeBltVKAtznefJGyRrEiXYu2NB+oH8e9+gGT3rc4CBPcc7Qh5p/lTdEnIXvTIe/hnwa9hzKmGK6iSMWJVDpA6jJkR1ECkPjTTFUc9RRzqFxQIgeL7ieiABYCqJBKO8Z4sXWSD4cJ49OpjI2J/Q6OsTSq7lQ74gDsc+RRetiRKEiPOby1mI1gWdNuvCSlrAgMBAAECggEAb+vYWiH+5/3UPNHKpoeIOISlVId0pCWLfqMQDh0zRwBYsTVkQCZrVWf+xAaYGyMVjwCQRV8KRbJcjYjbcHucSKogJOtJgNwUqZ6sjwy6jxVxlN/xzN9p8BBsez/sPvjaxv9uBPjtBEDY2cVHvcjRUVffphKpD6yA3Hherrh+5zFFLKUTkAreU9IM+sJv/KtliTL7YOpZdzq/fF+NSQnkDM4pKchQRZ90KtBAW7BV4Yi8fTxqcQdZrR5UIyxTP7RrFsZAXnXSdzigX/yYXkWJ+g0RwTmYOH9hSIU9HYBwquVrKfMu87RVQKbjMhUowNZ/EByIrZTi0d17qyUORngegQKBgQDFxmp2VFnDeCp55fvrakmob1+0Fmba+kVn4Y9Mpr5Otd3rXaXir7a+JFboJmOLUhpnKshJpSzOmw4/7Fh9qUZNppnsQcFl6yxqKXFfYGC8ymF/j9UpASBFd7IM+NuEZIOnW7ZR2TWPaUJncfJev3oNUo8G8OsS3TsTxsuJuiYgjQKBgQCqRctvaVEDh0GdpRTX4EXMcpQy2M6h/oVbM1Aeg+GGdIa8yYhS6YwkMPYfpBHH8bKSX1qkktgGeSxsxwBplk+QQk6yhCu7hDsBybNid/o+VjJ69KEXXCfX6MUUWVK/a6wfzDnh41AawLI9VR2m5yc6aqP7P6qqxlEHYAoFSY3f1wKBgGAH/f3C62scWBOkdnRSwb1FhwTcs8i0KStNvNUNSe7SJBV6/WdaxrMA1ioXKdKmZSefAG2yBFW1uG+VhUpO/VILCUZMj2m7jW9m/CPBmndRFZJo9qnYYCEbA5uE4qIgZETJJXgMrhMamyG7lGIpO0xowAYwyRP51AvARWh0uLqpAoGAXzqLrXK8UXna87z47EdNIivOwKVK44TCtp/7JiXiAt224WzhJmXh7/fGHyoWm7mMgpp7ymEqGmy4sNmXXHYpZ2f+NY8AGm64xaY2AlNRjH2O2/9m/PaYfgN7/YZ7Zf1lFZ+Vm8US7vNofC7XV5od8Wjk70bLP5mMhdBrqktZ4p8CgYEAjiVHvgyRTQOe/D7K5NIrcsifzXwbfrV0y36u2MMRPWr+9DdWrfnxpUYkkcPBYZYidEtH9LSb/yjmktnAwSXeqO4+1mXDUmg5GDDbtePmQU2/cZu+1zJF5ogN+37UCB3gJYtmbB5N2PxOCkxanO1Yaj4b6L1TTBwgrOflZHdKsLs=";
+    String alipayPublicKey = "MIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEA4gGimZ8dqRiPbx1vcg0nKeA/QeGGLnmmOfhNXq7bomSwuz2KPcTjmHQExG1HTbtrp6YL1t+lu8cQbPue2WS7GZAhwTwsFWzm7QCWzgHVk0ty00GSTxNufeA/bPXcTBELMoAANRUYiUl9Ccl1cV8IARbmA9r8HL05su/9pbDpWURYvlCTVIvUR5Avievd5S88H4y5QY7N7CShLKmEOQhIDXxlsVMRYv/q4sqMcXmDqoWRuF33om68LbW1rBeRMh0ePFMyBSsBBhmn/tshkiv6d8RlTj0LenrBE+7DF+IAP2aYqFgn/wGnDiqx6/dwn5p9IImNbmmR1SyJc4EbEIzrTwIDAQAB";
     AlipayConfig alipayConfig = new AlipayConfig();
 
     @PostMapping("/precreate")
     public Result precreate(@RequestBody Order createOrder) throws AlipayApiException {
+        Map<String, Object> responseMap = new HashMap<>();
         String phoneNumber = createOrder.getPhoneNumber();
         String inviteIdentifier = createOrder.getInviteIdentifier();
         log.info("{}请求生成订单，携带{}邀请码", phoneNumber, inviteIdentifier);
-        alipayConfig.setServerUrl("https://openapi-sandbox.dl.alipaydev.com/gateway.do");
-        alipayConfig.setAppId("9021000137686466");
+        alipayConfig.setServerUrl("https://openapi.alipay.com/gateway.do");
+        alipayConfig.setAppId("2021004148629471");
         alipayConfig.setPrivateKey(privateKey);
         alipayConfig.setFormat("json");
         alipayConfig.setAlipayPublicKey(alipayPublicKey);
@@ -41,14 +50,27 @@ public class AlipayController {
         request.setBizModel(model);
         AlipayTradePrecreateResponse response = alipayClient.execute(request);
         System.out.println(response.getBody());
+
+        String httpBody = response.getBody();
+        JsonObject jsonObject = JsonParser.parseString(httpBody).getAsJsonObject();
+        String alipayQRCode = jsonObject.getAsJsonObject("alipay_trade_precreate_response").get("qr_code").getAsString();
+        System.out.println(alipayQRCode);
         if (response.isSuccess()) {
             System.out.println("调用成功");
+//            String filePath = "/Users/cyp/Desktop/qrCode/qrcode.png";
+//            try {
+//                QRCodeUtil.generateQRCodeImage(alipayQRCode, 350, 350, filePath);
+//                responseMap.put("qrCodePath", filePath);
+//                responseMap.put("success", true);
+//            } catch (WriterException | IOException e) {
+//                throw new RuntimeException(e);
+//            }
         } else {
             System.out.println("调用失败");
             // sdk版本是"4.38.0.ALL"及以上,可以参考下面的示例获取诊断链接
             // String diagnosisUrl = DiagnosisUtils.getDiagnosisUrl(response);
             // System.out.println(diagnosisUrl);
         }
-        return null;
+        return new Result(1, "success get qrcode link" , alipayQRCode);
     }
 }
---
+++ src/main/java/cn/wowtw_backend/utils/QRCodeUtil.java
@@ -0,0 +1,25 @@
+package cn.wowtw_backend.utils;
+
+import com.google.zxing.BarcodeFormat;
+import com.google.zxing.EncodeHintType;
+import com.google.zxing.WriterException;
+import com.google.zxing.client.j2se.MatrixToImageWriter;
+import com.google.zxing.common.BitMatrix;
+import com.google.zxing.qrcode.QRCodeWriter;
+
+import java.io.IOException;
+import java.nio.file.FileSystems;
+import java.nio.file.Path;
+import java.util.HashMap;
+import java.util.Map;
+
+public class QRCodeUtil {
+    public static void generateQRCodeImage(String text, int width, int height, String filePath) throws WriterException, IOException {
+        QRCodeWriter qrCodeWriter = new QRCodeWriter();
+        Map<EncodeHintType, String> hints = new HashMap<>();
+        hints.put(EncodeHintType.CHARACTER_SET, "UTF-8");
+        BitMatrix bitMatrix = qrCodeWriter.encode(text, BarcodeFormat.QR_CODE, width, height, hints);
+        Path path = FileSystems.getDefault().getPath(filePath);
+        MatrixToImageWriter.writeToPath(bitMatrix, "PNG", path);
+    }
+}
