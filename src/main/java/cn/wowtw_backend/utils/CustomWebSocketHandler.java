6be49741c589cab02d72cce246db5d77925fea67
MCYP_MacStudio <shhcyp@qq.com>
2024-07-24 01:52:57 +0800

    正式完成websocket

--- src/main/java/cn/wowtw_backend/utils/CustomWebSocketHandler.java
+++ src/main/java/cn/wowtw_backend/utils/CustomWebSocketHandler.java
@@ -6,10 +6,30 @@ import org.springframework.web.socket.handler.TextWebSocketHandler;
 
 import java.util.Objects;
 import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.TimeUnit;
 
 public class CustomWebSocketHandler extends TextWebSocketHandler {
 
     private static final ConcurrentHashMap<String, WebSocketSession> sessions = new ConcurrentHashMap<>();
+    private static final ScheduledExecutorService scheduler = Executors.newScheduledThreadPool(1);
+
+    public CustomWebSocketHandler() {
+        scheduler.scheduleAtFixedRate(this::sendPingMessages, 0, 30, TimeUnit.SECONDS);  // 每30秒发送一次心跳消息
+    }
+
+    private void sendPingMessages() {
+        sessions.values().forEach(session -> {
+            try {
+                if (session.isOpen()) {
+                    session.sendMessage(new PingMessage());
+                }
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        });
+    }
 
     @Override
     public void afterConnectionEstablished(WebSocketSession session) throws Exception {
--- src/main/java/cn/wowtw_backend/utils/JwtUtils.java
+++ src/main/java/cn/wowtw_backend/utils/JwtUtils.java
@@ -10,10 +10,10 @@ import java.util.Map;
 public class JwtUtils {
     private static final String SECRET_KEY = "gtORUQljR76IC6MzysLBR8hZOZMCqswGF0jymCRkixE="; // 确保这里的密钥与验证时使用的密钥一致
     private static final SecretKey key = Keys.hmacShaKeyFor(SECRET_KEY.getBytes());
-    private static final Date expirationDate = new Date(System.currentTimeMillis() + 43200000L);
 
     // 生成 JWT
     public static String generateJwt(String username, Map<String, Object> claims) {
+        Date expirationDate = new Date(System.currentTimeMillis() + 43200000L);  // 每次生成JWT时重新计算过期时间
         return Jwts.builder()
                 .addClaims(claims)
                 .setSubject(username) // Subject 可以根据实际需要设置
